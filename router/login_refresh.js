13129a32b898daf5a7f5afa09afcf7d13fa23b1a
zbhgit <zbh_wj@163.com>
2018-02-28 13:40:48 +0800

    change server

--- router/login_refresh.js
+++ router/login_refresh.js
@@ -1,28 +1,26 @@
-const express = require('express')
-const router = express()
-const { createWebAPIRequest } = require('../util/util')
+const express = require("express");
+const router = express();
+const { createWebAPIRequest } = require("../util/util");
 
-router.get('/', (req, res) => {
-  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+router.get("/", (req, res) => {
+  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
   const data = {
-    csrf_token: ''
-  }
-  console.log({ cookie })
+    csrf_token: ""
+  };
   createWebAPIRequest(
-    'music.163.com',
+    "music.163.com",
     `/weapi/login/token/refresh`,
-    'POST',
+    "POST",
     data,
     cookie,
     (music_req, cookie) => {
-      console.log({ cookie })
       res.set({
-        'Set-Cookie': cookie
-      })
-      res.send(music_req)
+        "Set-Cookie": cookie
+      });
+      res.send(music_req);
     },
-    err => res.status(502).send('fetch error')
-  )
-})
+    err => res.status(502).send("fetch error")
+  );
+});
 
-module.exports = router
+module.exports = router;
--- router/playlist_detail.js
+++ router/playlist_detail.js
@@ -1,67 +1,31 @@
-const http = require('http')
-const express = require('express')
-const router = express()
-const { createWebAPIRequest } = require('../util/util')
+const http = require("http");
+const express = require("express");
+const router = express();
+const { createWebAPIRequest } = require("../util/util");
 
-router.get('/', (req, res) => {
-  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
-  let detail, imgurl
+router.get("/", (req, res) => {
+  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
   const data = {
     id: req.query.id,
-    offset: 0,
-    total: true,
-    limit: 1000,
-    n: 1000,
-    csrf_token: ''
-  }
+    n: 100000,
+    csrf_token: ""
+  };
 
   createWebAPIRequest(
-    'music.163.com',
-    '/weapi/v3/playlist/detail',
-    'POST',
+    "music.163.com",
+    `/api/playlist/detail?id=${req.query.id}`,
+    "POST",
     data,
     cookie,
     music_req => {
-      console.log(music_req)
-      // detail = music_req
-      res.send(music_req)
-      // mergeRes()
+      // console.log(JSON.parse(music_req).playlist.tracks.length)
+      // console.log(JSON.parse(music_req).playlist.trackIds.length)
+      res.send(music_req);
     },
     err => {
-      res.status(502).send('fetch error')
+      res.status(502).send("fetch error");
     }
-  )
+  );
+});
 
-  // FIXME:i dont know the api to get coverimgurl
-  // so i get it by parsing html
-  // const http_client = http.get({
-  //   hostname: 'music.163.com',
-  //   path: '/playlist?id=' + req.query.id,
-  //   headers: {
-  //     'Referer': 'http://music.163.com',
-  //   },
-  // }, function (res) {
-  //   res.setEncoding('utf8')
-  //   let html = ''
-  //   res.on('data', function (chunk) {
-  //     html += chunk
-  //   })
-  //   res.on('end', function () {
-  //     console.log('end', html)
-  //     const regImgCover = /\<img src=\"(.*)\" class="j-img"/ig
-  //     imgurl = regImgCover.exec(html)[1]
-  //     mergeRes()
-
-  //   })
-  // })
-
-  // function mergeRes() {
-  //   if (imgurl != undefined && detail != undefined) {
-  //     detail = JSON.parse(detail)
-  //     detail.playlist.picUrl = imgurl
-  //     res.send(detail)
-  //   }
-  // }
-})
-
-module.exports = router
+module.exports = router;
